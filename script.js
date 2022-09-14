f2a1198c5ebf14df3d1c7e420d0a6a0f8de8baae
egitaro <moekichi5525@gmail.com>
2022-09-14 22:32:42 +0900

    JSつけてます

--- script.js
+++ script.js
@@ -13,6 +13,7 @@
 //   }
 // );
 
+
 // 全部動いた！
 var btn = document.getElementsByClassName('btn');
 for (var i = 0; i < btn.length; i++) {
@@ -30,6 +31,7 @@ for (var i = 0; i < btn.length; i++) {
 };
 
 
+
 // FLOW slickプラグイン
 $('.slider').slick({
   autoplay: true,//自動的に動き出すか。初期値はfalse。
@@ -42,4 +44,102 @@ $('.slider').slick({
   centerMode: true,//要素を中央ぞろえにする
   variableWidth: true,//幅の違う画像の高さを揃えて表示
   dots: true,//下部ドットナビゲーションの表示
+});
+
+
+
+// link スクロールで出てくる
+$(function () {
+  $(window).scroll(function () { //windowがスクロールされたら以下の処理を実行する
+    $('#link').each(function () {
+      var position = $(this).offset().top;
+      //.offset().topで、ページの最上部から「＃link」が付いた要素までの距離を取得して、変数positionに代入
+
+
+      var scroll = $(window).scrollTop();
+      //.scrollTop()で、スクロールした量を取得して、変数scrollに代入
+      var windowHeight = $(window).height();
+      //.height()で、ウィンドウの高さを取得して、変数windowHeightに代入
+
+
+      if (scroll > position - windowHeight + 1300) {
+        $(this).addClass('active');
+        //スクロール量が要素の位置を過ぎた瞬間、「#link」が付いた要素にactiveクラスが付く
+      }
+    });
+  });
+});
+
+
+// 動きのきっかけとなるアニメーションの名前を定義
+function strengthAnime() {
+
+  //ふわっと動くきっかけのクラス名と動きのクラス名の設定
+  $('.fadeUpTrigger').each(function () { //fadeInUpTriggerというクラス名が
+    var elemPos = $(this).offset().top - 50; //要素より、50px上の
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight) {
+      $(this).addClass('fadeUp');
+      // 画面内に入ったらfadeInというクラス名を追記
+    }
+
+    //  else {
+    //   $(this).removeClass('fadeUp');
+    //   // 画面外に出たらfadeInというクラス名を外す
+    // }
+
+  });
+
+};
+
+//strength img 動くけど長いし何度も反応する
+// $(function () {
+//   $(window).scroll(function () { //windowがスクロールされたら以下の処理を実行する
+//     $('.strength-img img').hide().each(function () {
+//       var position = $(this).offset().top;
+//       //.offset().topで、ページの最上部から「＃strength」が付いた要素までの距離を取得して、変数positionに代入
+
+//       var scroll = $(window).scrollTop();
+//       //.scrollTop()で、スクロールした量を取得して、変数scrollに代入
+//       var windowHeight = $(window).height();
+//       //.height()で、ウィンドウの高さを取得して、変数windowHeightに代入
+
+//       if (scroll > position - windowHeight + 1000) {
+//         $('.strength-img img').fadeIn();
+//       }
+//     });
+//   });
+// });
+
+
+//strength img ぐるりん
+$(function () {
+  $('.strength-img img').hide();
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 1100) {
+      $('.strength-img img').fadeIn();
+    }
+  });
+});
+
+
+//reason img くるりん
+$(function () {
+  $('.reason-img img').hide();
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 3700) {
+      $('.reason-img img').fadeIn();
+    }
+  });
+});
+
+//commodity img くるりん
+$(function () {
+  $('.commodity-img-box').hide();
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 3000) {
+      $('.delay01').fadeIn();
+    }
+  });
 });
